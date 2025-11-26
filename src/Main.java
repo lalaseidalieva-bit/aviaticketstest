4a756cc98e95be9b59a7a85e0a59dead2bd52656
Lala Seyid <lalaseidalieva@gmail.com>
2025-11-26 12:11:29 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       int ticketPrice = 5_000;
+       int rublesPerMile = 20;
+
+       int miles = ticketPrice / rublesPerMile;
+
+       System.out.println("Начислено миль: " + miles);
+
+
+    }
+    }
