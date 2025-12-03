d395e97e3614ff855c64bc60fcbde4fb1528b248
Lala Seyid <lalaseidalieva@gmail.com>
2025-12-03 09:04:33 +0400

    changes

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       int ticketPrice = 5_000;
-       int rublesPerMile = 20;
+        int ticketPrice = 5_000;
+        int rublesPerMile = 20;
 
-       int miles = ticketPrice / rublesPerMile;
+        int miles = ticketPrice / rublesPerMile;
 
-       System.out.println("Начислено миль: " + miles);
+        System.out.println("Начислено миль: " + miles);
 
 
     }
-    }
+}
